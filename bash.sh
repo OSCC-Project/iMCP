bd58da04ce78abebb85ea5dc7979bffa403e212e
simintao <simintao@126.com>
2025-08-01 17:59:03 +0800

    refactor: update bash

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
-docker build -t mcp-ieda:1.0
-
+#!/bin/bash
+set -e
+docker build -t mcp-ieda:1.0 .
 docker run -v  /lib/x86_64-linux-gnu/libgomp.so.1:/lib/x86_64-linux-gnu/libgomp.so.1 -v /lib/x86_64-linux-gnu/libunwind.so.8:/lib/x86_64-linux-gnu/libunwind.so.8  -p 3002:3002 -it --rm mcp-ieda:1.0
